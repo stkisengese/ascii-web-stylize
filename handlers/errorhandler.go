83d71d467b6475cd0ec1ad9b2345c4d95bdc6779
skisenge <skisengese@gmail.com>
2024-07-25 01:19:53 +0300

    style: change code format on the errorData stuct.

--- handlers/errorhandler.go
+++ handlers/errorhandler.go
@@ -6,23 +6,39 @@ import (
 	"net/http"
 )
 
+// ErrorData represents data structure for the error page template.
+type ErrorData struct {
+	Code int
+	Str  string
+}
+
+// ErrorHandler represents an error handler that handles HTTP errors.
 func ErrorHandler(w http.ResponseWriter, str string, code int) {
+	// validate the code
+	if code < http.StatusBadRequest || code >= http.StatusInternalServerError {
+		http.Error(w, "Error 500: Internal server error", http.StatusInternalServerError)
+		log.Println("Error: Invalid status code.")
+		return
+	}
+
+	// Parse the template for error page.
 	varerr, err := template.ParseFiles("../templates/error.html")
 	if err != nil {
 		http.Error(w, "Error 500: Internal server error", http.StatusInternalServerError)
-		log.Fatal("Error", err)
+		log.Println("Error parsing the error template.")
 		return
 	}
-	Person := struct {
-		Code int
-		Str  string
-	}{
-		Code: code,
-		Str:  str,
-	}
+
+	// Define the data for the template.
+	data := ErrorData{code, str}
+
+	// Write the template.
 	w.WriteHeader(code)
-	err = varerr.Execute(w, Person)
+
+	// Execute the template with the data.
+	err = varerr.Execute(w, data)
 	if err != nil {
-		log.Fatal("Error", err)
+		log.Println("Error Executing error template.")
+		ErrorHandler(w, "Error 500: Internal server error", http.StatusInternalServerError)
 	}
 }
